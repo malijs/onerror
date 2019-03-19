7bd0fa7bade66cd1d34a18098a0dd43c6d3f0fcb
Bojan <dbojan@gmail.com>
2019-03-18 21:53:03 -0300

    Set req and res types to any in type definitions. Fixes #13.

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,7 @@
 import { Context } from "mali";
 
-declare function onError(
+declare function onError (
     fn: (err: Error, ctx: Context) => any
 ): (ctx: Context, next?: () => Promise<any>) => Promise<void>;
 
 export = onError;
-
-declare function compose<T>(middleware: Array<compose.Middleware<T>>): compose.ComposedMiddleware<T>;
