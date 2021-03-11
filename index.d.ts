165a5d8df4ab0ac5fac869c0f9a413986f404821
John Easton <john@easton.dev>
2021-03-11 10:31:34 +0000

    fix: outdated types

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
-import { Context } from "mali";
+import type { Context } from "mali";
 
-declare function onError (
-    fn: (err: Error, ctx: Context) => any
-): (ctx: Context, next?: () => Promise<any>) => Promise<void>;
+declare function onError(
+  fn: (err: Error, ctx: Context<any>) => any
+): (ctx: Context<any>, next?: () => Promise<any>) => Promise<void>;
 
 export = onError;
